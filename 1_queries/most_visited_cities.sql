c923ae4b0f40207817cea761c68a26e2e1c9991f
ZakiyaA <zakiyaalfughi@gmail.com>
2022-01-06 07:45:43 +0000

    Add queries

---
+++ 1_queries/most_visited_cities.sql
@@ -0,0 +1,5 @@
+SELECT city as name, COUNT(reservations.id) as total_reservations
+FROM properties
+JOIN reservations ON property_id = properties.id
+GROUP BY city
+ORDER BY total_reservations DESC;
